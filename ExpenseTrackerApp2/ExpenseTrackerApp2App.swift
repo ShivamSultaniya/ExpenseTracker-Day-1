1db8f3cd74089ebb1899d0aa9fca195ce7537ad2
ShivamSultaniya <142833938+ShivamSultaniya@users.noreply.github.com>
2024-06-08 14:26:16 +0545

    Initial Commit

---
+++ ExpenseTrackerApp2/ExpenseTrackerApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp2App.swift
+//  ExpenseTrackerApp2
+//
+//  Created by Shivam Sultaniya on 08/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
