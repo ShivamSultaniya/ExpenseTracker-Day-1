49517f8ea469bc6c921415942f225fc9c279aa9e
ShivamSultaniya <142833938+ShivamSultaniya@users.noreply.github.com>
2024-06-09 22:17:47 +0545

    Day 1 Content

---
+++ ExpenseTrackerApp2/Model/Tab.swift
@@ -0,0 +1,18 @@
+//
+//  Tab.swift
+//  ExpenseTrackerApp2
+//
+//  Created by Shivam Sultaniya on 08/06/24.
+//
+
+import SwiftUI
+
+struct Tab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tab()
+}
---
+++ ExpenseTrackerApp2/Model/Transaction.swift
@@ -0,0 +1,18 @@
+//
+//  Transaction.swift
+//  ExpenseTrackerApp2
+//
+//  Created by Shivam Sultaniya on 08/06/24.
+//
+
+import SwiftUI
+
+struct Transaction: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Transaction()
+}
